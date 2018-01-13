80ca035431fccf1308d726560475a64148025703
Ronald Young <ronnie.kingsley.young@gmail.com>
2018-01-13 17:36:31 +0000

    automatically backed up by learn

--- beyoncesAccountant.js
+++ beyoncesAccountant.js
@@ -1,8 +1,8 @@
 console.log("Dear Beyonce,")
 console.log("We have calculated your total earnings this year.")
 console.log("You have earned " + beyoncesMoney)
-console.log("Of" totalSum " some portion derives from your live performances.")
-console.log("Of"totalSum " some portion derives from music sales.")
+console.log("Of" + totalSum + " some portion derives from your live performances.")
+console.log("Of" + totalSum + " some portion derives from music sales.")
 var totalSum = 500 + 750 + 3000 + 5100 + 12980 + 9087 + 50000; 
 
 // DON'T DO THIS PART YET:
